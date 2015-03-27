e0d4c741f8a060470ac84c296a6fa5a4585594c3
Shaw Innes <shaw@immortal.net.au>
2015-03-27 12:20:21 +1100

    Add Swagger API Documentation Project

---
+++ SwaggerDemo/Controllers/NewsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using SwaggerDemo.Models;
+
+namespace SwaggerDemo.Controllers
+{
+    public class NewsController : ApiController
+    {
+        public IEnumerable<NewsItem> Get()
+        {
+            return new List<NewsItem>
+            {
+                new NewsItem
+                {
+                    Id = 1,
+                    Title = "Hello World #1",
+                    Content = "Lorem Ipsum...",
+                    Published = DateTimeOffset.UtcNow
+                },
+                new NewsItem
+                {
+                    Id = 2,
+                    Title = "Hello World #2",
+                    Content = "Lorem Ipsum...",
+                    Published = DateTimeOffset.UtcNow
+                }
+            };
+        }
+
+        public NewsItem Get(int id)
+        {
+            return new NewsItem {Id = id};
+        }
+    }
+}
---
+++ SwaggerDemo/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Http.Formatting;
+using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using SwaggerDemo;
+using Swashbuckle.Application;
+
+[assembly: OwinStartup(typeof (Startup))]
+
+namespace SwaggerDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new {id = RouteParameter.Optional}
+                );
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings =
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+
+            config
+                .EnableSwagger(c =>
+                {
+                    c.SingleApiVersion("v1", "A title for your API");
+
+                    c.BasicAuth("basic")
+                        .Description("Basic HTTP Authentication");
+
+                    c.ApiKey("apiKey")
+                        .Description("API Key Authentication")
+                        .Name("apiKey")
+                        .In("header");
+                })
+                .EnableSwaggerUi();
+
+            var builder = new ContainerBuilder();
+
+            // Register API controllers using assembly scanning.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Run other optional steps, like registering filters,
+            // per-controller-type services, etc., then set the dependency resolver
+            // to be Autofac.
+            var container = builder.Build();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            // Register the Autofac middleware FIRST, then the Autofac Web API middleware,
+            // and finally the standard Web API middleware.
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+        }
+    }
+}
