2d464e6eca79174988bba185842dd86431f2f26a
Shaw Innes <shaw.innes@tattsgroup.com>
2015-06-09 19:54:28 +1000

    Pizza Bonus Tests

---
+++ TestabilityTests/BonusPizza.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+
+namespace TestabilityTests
+{
+    [TestFixture]
+    public class BonusPizza
+    {
+        [Test]
+        public void FullPrice()
+        {
+            var price = string.Empty;
+
+            var day = DateTime.Now.DayOfWeek;
+            if (day == DayOfWeek.Tuesday)
+            {
+                price = "half price";
+            }
+            else if (day != DayOfWeek.Tuesday)
+            {
+                price = "full price";
+            }
+
+            price.ShouldBe("full price");
+        }
+
+        [Test]
+        public void HalfPriceOnTuesdays()
+        {
+            var price = string.Empty;
+         
+            var day = DateTime.Now.DayOfWeek;
+            if (day == DayOfWeek.Tuesday)
+            {
+                price = "half price";
+            }
+            else if (day != DayOfWeek.Tuesday)
+            {
+                price = "full price";
+            }
+
+            price.ShouldBe("half price");
+        }
+    }
+}
